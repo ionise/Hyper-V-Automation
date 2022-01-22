f19a41410ea012e9dfc15429c00a74d0951baad4
David Alderman <david.alderman@vuw.ac.nz>
2022-01-22 21:18:17 +1300

    Modified for my own repo

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,10 +1,10 @@
 # Enables TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$url = 'https://codeload.github.com/fdcastel/Hyper-V-Automation/zip/master'
-$fileName = Join-Path $env:TEMP 'Hyper-V-Automation-master.zip'
+$url = 'https://codeload.github.com/ionise/Hyper-V-Automation/zip/main'
+$fileName = Join-Path $env:TEMP 'Hyper-V-Automation-main.zip'
 
 Invoke-RestMethod $url -OutFile $fileName
 Expand-Archive $fileName -DestinationPath $env:TEMP -Force
 
-cd "$env:TEMP\Hyper-V-Automation-master"
+cd "$env:TEMP\Hyper-V-Automation-main"
